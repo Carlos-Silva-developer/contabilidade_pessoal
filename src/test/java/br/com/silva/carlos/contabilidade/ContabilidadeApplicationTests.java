15e9ecb33e148e51cf8d74a7256698406186e053
Carlos Darci Melo da Silva <devcarlosdms@gmail.com>
2022-12-29 19:07:33 -0300

    comit da base do projeto

---
+++ src/test/java/br/com/silva/carlos/contabilidade/ContabilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.silva.carlos.contabilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
