ee131ee1f3ec72c9917568024909074697a606f3
Carlos Darci Melo da Silva <devcarlosdms@gmail.com>
2023-07-26 15:49:30 -0300

    adicionando configs do banco de dados

--- src/main/java/br/com/silva/carlos/contabilidade/domain/Bills.java
+++ src/main/java/br/com/silva/carlos/contabilidade/domain/Bills.java
@@ -3,20 +3,23 @@ package br.com.silva.carlos.contabilidade.domain;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Entity
 public class Bills implements Serializable{
 	
+	@Id
 	private static final long serialVersionUID = 1L;
 	private String name;
-	private Double value;
-	private Double taxes;
+	private double value;
+	private double taxes;
 	private LocalDate paymentDay;
 	private LocalDate paymentLimt;
 	
--- src/main/java/br/com/silva/carlos/contabilidade/domain/Person.java
+++ src/main/java/br/com/silva/carlos/contabilidade/domain/Person.java
@@ -17,32 +17,9 @@ public class Person implements Serializable{
 	
 	@SuppressWarnings("unused")
 	private String name;
-	private List<Profit> income;
-	private List<Bills> outcome;
+	private List<Double> income;
+	private List<Double> outcome;
 	
 	
 
-	public Double calculateIncome() {		
-		double returnValue = 0;		
-		
-		for (Double double1 : income) {
-			returnValue += double1;
-		}		
-		
-		return returnValue;
-	}
-	
-	public Double calculateOutcome() {		
-		double returnValue = 0;		
-		
-		for (Double double1 : outcome) {
-			returnValue += double1;
-		}		
-		
-		return returnValue;
-	}
-
-	public Double calculateProfit() {
-		return calculateIncome() - calculateOutcome();
-	}
-}
+	)
